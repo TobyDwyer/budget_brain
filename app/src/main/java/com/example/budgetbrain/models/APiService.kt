0f477d59a7baa2af12dd9cfb381d3880e783c82a
Brandon <brandoncalitz52@gmail.com>
2024-09-30 13:38:50 +0200

    auth working

--- app/src/main/java/com/example/budgetbrain/models/APiService.kt
+++ app/src/main/java/com/example/budgetbrain/models/APiService.kt
@@ -1,9 +1,11 @@
+import com.example.budgetbrain.models.BudgetItem
 import com.example.budgetbrain.models.SessionUser
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import java.util.Date
 
 data class LoginRequest(val email: String, val password: String)
 data class LoginResponse(val token: String)
@@ -17,11 +19,22 @@ data class RegisterRequest(
     val languagePreference: String,
     val savingsGoal: Double
 )
+data class RegisterResponse(val token: String)
 
 data class UserResponse(val user: SessionUser)
 
-data class RegisterResponse(val token: String)
+data class BudgetCreateRequest(
+    val name: String,
+    val startDate: Date,
+    val endDate: Date,
+    val budgetedAmount: Double,
+    val categories: List<String> = emptyList(),
+)
+data class BudgetCreateResponse( val budget : BudgetItem)
+
+
 interface ApiService {
+    // -------- AUTH ---------
     @POST("auth/login")
     fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 
@@ -30,6 +43,10 @@ interface ApiService {
 
     @POST("auth/user")
     fun user(): Call<UserResponse>
+
+    // -------- BUDGETS ---------
+    @POST("budgets")
+    fun budgetCreate(@Body budgetCreateRequest: BudgetCreateRequest): Call<BudgetCreateResponse>
 }
 
 
